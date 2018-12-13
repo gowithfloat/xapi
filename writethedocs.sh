dd63317df9003b89cb9933d6e05340c525563831
Steve Richey <srichey@gowithfloat.com>
2018-12-13 11:22:56 -0600

    add script for generating documentation

---
+++ writethedocs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set +euxr
+
+input_path="Float.xAPI/bin/Debug/netstandard2.0/Float.xAPI.XML"
+output_path="docs/readme.md"
+
+if [ ! -f "${input_path}" ]; then
+  echo "Build the xAPI project first to generate XML file."
+  exit 1
+fi
+
+if [ ! -f "Float.VSDocGen/bin/Debug/Float.VSDocGen.exe" ]; then
+  echo "Building VSDocGen executable..."
+  msbuild Float.VSDocGen/Float.VSDocGen.fsproj
+fi
+
+echo "Generating documentation..."
+mono Float.VSDocGen/bin/Debug/Float.VSDocGen.exe \
+  "${input_path}" \
+  "${output_path}"
+
+echo "Done."
