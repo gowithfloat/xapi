779add41aca446e2de8112d9aace410cc680b094
Steve Richey <srichey@gowithfloat.com>
2019-01-16 02:29:15 -0600

    organize openid tests

--- Float.xAPI.Tests/OpenID.Tests.cs
+++ Float.xAPI.Tests/OpenID.Tests.cs
@@ -9,15 +9,12 @@ using Xunit;
 
 namespace Float.xAPI.Tests
 {
-    public class OpenIDTests : IInitializationTests<OpenID>, IEqualityTests
+    public class OpenIDTests : IInitializationTests<OpenID>, IEqualityTests, IPropertyTests
     {
         [Fact]
         public OpenID TestValidInit()
         {
-            var openid = new OpenID(new Uri("http://openid.com/jane-schmoe"));
-            Assert.Equal("http://openid.com/jane-schmoe", openid.OpenID.AbsoluteUri);
-            Assert.Equal(new Uri("http://openid.com/jane-schmoe"), (openid as IOpenID).OpenID);
-            return openid;
+            return new OpenID(new Uri("http://openid.com/jane-schmoe"));
         }
 
         [Fact]
@@ -42,11 +39,15 @@ namespace Float.xAPI.Tests
         {
             var openid1 = new OpenID(new Uri("http://openid.com/sue-schmoe"));
             var openid2 = new OpenID(new Uri("http://openid.com/jane-schmoe"));
-            Assert.NotEqual(openid1, openid2);
-            Assert.True(openid1 != openid2);
-            Assert.False(openid1 == openid2);
-            Assert.NotEqual(openid1.GetHashCode(), openid2.GetHashCode());
-            Assert.NotEqual(openid1.OpenID, openid2.OpenID);
+            AssertHelper.Inequality<OpenID, IOpenID, IInverseFunctionalIdentifier>(openid1, openid2, (a, b) => a != b);
+        }
+
+        [Fact]
+        public void TestProperties()
+        {
+            var openid = new OpenID(new Uri("http://openid.com/jane-schmoe"));
+            Assert.Equal("http://openid.com/jane-schmoe", openid.OpenID.AbsoluteUri);
+            Assert.Equal(new Uri("http://openid.com/jane-schmoe"), (openid as IOpenID).OpenID);
         }
     }
 }
