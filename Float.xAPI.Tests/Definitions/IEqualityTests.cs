98e52d2a7b869df41cdc8ab9f45ad2cd0c6a759f
Steve Richey <srichey@gowithfloat.com>
2019-01-16 02:28:21 -0600

    todo: equality tests could be simpler

--- Float.xAPI.Tests/Definitions/IEqualityTests.cs
+++ Float.xAPI.Tests/Definitions/IEqualityTests.cs
@@ -3,6 +3,8 @@
 // Shared under an MIT license. See license.md for details.
 // </copyright>
 
+// todo: tests should verify that all types in the xAPI namespace that are equatable are tested
+// todo: since most of these will just defer to the assert helper, provide a default implementation that does that
 namespace Float.xAPI.Tests
 {
     /// <summary>
