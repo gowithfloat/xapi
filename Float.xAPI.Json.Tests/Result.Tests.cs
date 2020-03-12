4af7e67c9e94f04588fce316fd0c5d3b495bddd1
Steve Richey <srichey@gowithfloat.com>
2020-03-12 14:23:28 -0500

    add result deserialization tests

--- Float.xAPI.Json.Tests/Result.Tests.cs
+++ Float.xAPI.Json.Tests/Result.Tests.cs
@@ -37,5 +37,48 @@ namespace Float.xAPI.Json.Tests
             var json = Serialize.Result(result);
             Assert.Equal("{\"score\":{\"scaled\":0.74,\"raw\":37,\"min\":0,\"max\":50},\"success\":true,\"completion\":false,\"duration\":\"PT1H13M17S\"}", json);
         }
+
+        [Fact]
+        public void TestDeserializeEmpty()
+        {
+            var result = Deserialize.ParseResult("{}").Value;
+            Assert.Null(result.Completion);
+            Assert.Null(result.Duration);
+            Assert.Null(result.Extensions);
+            Assert.Null(result.Response);
+            Assert.Null(result.Score);
+            Assert.Null(result.Success);
+        }
+
+        [Fact]
+        public void TestDeserializeMinimal()
+        {
+            var result = Deserialize.ParseResult("{\"score\":{\"scaled\":0.23}}").Value;
+            Assert.Null(result.Completion);
+            Assert.Null(result.Duration);
+            Assert.Null(result.Extensions);
+            Assert.Null(result.Response);
+            Assert.NotNull(result.Score);
+            Assert.Equal(0.23, result.Score.Value.Scaled);
+            Assert.Null(result.Score.Value.Max);
+            Assert.Null(result.Score.Value.Min);
+            Assert.Null(result.Score.Value.Raw);
+            Assert.Null(result.Success);
+        }
+
+        [Fact]
+        public void TestDeserializeMaximal()
+        {
+            var result = Deserialize.ParseResult("{\"score\":{\"scaled\":0.74,\"raw\":37,\"min\":0,\"max\":50},\"success\":true,\"completion\":false,\"duration\":\"PT1H13M17S\"}").Value;
+            Assert.True(result.Success.Value);
+            Assert.False(result.Completion.Value);
+            Assert.Equal(1, result.Duration.Value.Hours);
+            Assert.Equal(13, result.Duration.Value.Minutes);
+            Assert.Equal(17, result.Duration.Value.Seconds);
+            Assert.Equal(0.74, result.Score.Value.Scaled);
+            Assert.Equal(37, result.Score.Value.Raw);
+            Assert.Equal(0, result.Score.Value.Min);
+            Assert.Equal(50, result.Score.Value.Max);
+        }
     }
 }
